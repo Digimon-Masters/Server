ff8f7ebf59baba8b2b8aa2e2f4b859a9899a4d6a
MasiusRZ <masiusrz@gmail.com>
2024-06-26 21:14:07 +0100

    Added base implementation for CashShopOnOpenPacketProcessor

---
+++ src/Source/Distribution/DigitalWorldOnline.Game.Host/PacketProcessors/CashShopOnOpenPacketProcessor.cs
@@ -0,0 +1,20 @@
+﻿using DigitalWorldOnline.Commons.Entities;
+using DigitalWorldOnline.Commons.Enums.PacketProcessor;
+using DigitalWorldOnline.Commons.Interfaces;
+using DigitalWorldOnline.Commons.Writers;
+
+namespace DigitalWorldOnline.Game.PacketProcessors;
+
+public class CashShopOnOpenPacketProcessor : IGamePacketProcessor
+{
+    public GameServerPacketEnum Type => GameServerPacketEnum.CashShopOnOpen;
+    
+    public async Task Process(GameClient client, byte[] packetData)
+    {
+        //TODO: Verify what more can we send here.
+        var packetWriter = new PacketWriter();
+        packetWriter.Type(3412);
+        client.Send(packetWriter);
+    }
+
+}
