6141e382860c0d96dcb816750c8daa8be27a2642
MasiusRZ <masiusrz@gmail.com>
2024-06-26 21:28:59 +0100

    Added Cash Shop repository

---
+++ src/Source/Domain/DigitalWorldOnline.Commons/Interfaces/ICashShopRepository.cs
@@ -0,0 +1,8 @@
+﻿using DigitalWorldOnline.Commons.DTOs.Shop;
+
+namespace DigitalWorldOnline.Commons.Interfaces;
+
+public interface ICashShopRepository
+{
+    Task<CashShopDTO?> GetCashShopItemAsync(int uniqueId);
+}
---
+++ src/Source/Infra/DigitalWorldOnline.Infraestructure/Repositories/Shop/CashShopRepository.cs
@@ -0,0 +1,21 @@
+﻿using DigitalWorldOnline.Commons.DTOs.Shop;
+using DigitalWorldOnline.Commons.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalWorldOnline.Infraestructure.Repositories.Shop;
+
+public class CashShopRepository : ICashShopRepository
+{
+    private readonly DatabaseContext _context;
+
+    public CashShopRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CashShopDTO?> GetCashShopItemAsync(int uniqueId)
+    {
+        var cashShopItem = await _context.CashShopItems.Include(x => x.CashShopItemsNavigation).FirstOrDefaultAsync(x => x.UniqueId == uniqueId);
+        return cashShopItem;
+    }
+}
